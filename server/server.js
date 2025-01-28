6fe9298c34f3ccd595e23f068afce20e9bcca0eb
Shalin Scupham <shalin.scupham@gmail.com>
2025-01-27 22:32:17 -0500

    actually use 4o mini

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.get('/session', async (req, res) => {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                model: 'gpt-4o-realtime-preview-2024-12-17',
+                model: 'gpt-4o-mini-realtime-preview-2024-12-17',
                 voice: 'shimmer',
             }),
         });
